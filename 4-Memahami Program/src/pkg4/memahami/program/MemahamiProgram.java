444a2377cc49871d3a39710de46f93e11d72b23f
fajarbhaktinugraha11 <fajarbhaktinugraha11@gmail.com>
2019-12-20 16:06:34 +0700

    fajar

---
+++ 4-Memahami Program/src/pkg4/memahami/program/MemahamiProgram.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg4.memahami.program;
+
+/**
+ * Nama : FAJAR BHAKTI NUGRAHA
+ * NIM  : A2.1900062
+ * Kelas: TI-1C
+ */
+public class MemahamiProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("hello world");
+    }
+}
---
+++ 5-Organisasi Program dengan Package/src/Program_IDE/OrganisasiProgramDenganPackage.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Program_IDE;
+
+/**
+ * Nama : FAJAR BHAKTI NUGRAHA
+ * NIM  : A2.1900062
+ * Kelas: TI-1C
+ */
+public class OrganisasiProgramDenganPackage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("hello world");
+    }
+}
